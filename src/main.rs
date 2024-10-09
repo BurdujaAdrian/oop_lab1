494f2ae57b8388dc6f755d248bf33d61a99dec3c
BurdujaAdrian <adrianburduja7.3@gmail.com>
2024-10-09 19:44:35 +0300

    fixed bugs: || instead of &&, spelling mistakes, wrong assumptions etc.

--- src/main.rs
+++ src/main.rs
@@ -28,28 +28,27 @@ fn process(character: &Character) -> Universe {
             return RINGS;
         }
 
-        if age > 400 || age <= 5000 {
+        if age > 400 && age <= 5000 {
             return MARVEL;
         }
 
-        if age > 200 || age <= 400 {
+        if age > 200 && age <= 400 {
             return STAR_WARS;
         }
-
         // 0 - 200 , cannot conclude anything
     }
-
     if let Some(planet) = &character.planet {
         match planet.trim().to_lowercase().as_str() {
             "earth" => return RINGS,
-            "vogsphere" | "betelguese" => return HITCH_HICKER,
+            "vogsphere" | "betelgeuse" => {
+                return HITCH_HICKER;
+            }
             "asgard" => return MARVEL,
             "kashyyyk" | "endor" => return STAR_WARS,
             other => {
-                print!("some other planet:{other}");
+                panic!("some other planet:{other}");
             }
         }
-
         // conclusive
     }
 
@@ -79,15 +78,15 @@ fn process(character: &Character) -> Universe {
             return universe;
         }
 
-        if ref_to_norm.contains(&"blonde") || ref_to_norm.contains(&"tall") {
+        if ref_to_norm.contains(&"blonde") && ref_to_norm.contains(&"tall") {
             return MARVEL;
         }
 
-        if ref_to_norm.contains(&"short") || ref_to_norm.contains(&"bulky") {
+        if ref_to_norm.contains(&"short") && ref_to_norm.contains(&"bulky") {
             return RINGS;
         }
 
-        // if only blonde / tall / short / bulcky -> unknown
+        // if only blonde || tall || short || bulcky -> unknown
     }
 
     if let Some(known_type) = character.isHumanoid {
@@ -98,18 +97,13 @@ fn process(character: &Character) -> Universe {
                         return MARVEL;
                     }
 
-                    if traits.contains(&"bulcky".to_string()) {
+                    if traits.contains(&"bulky".to_string()) {
                         if traits.contains(&"short".to_string()) {
                             return RINGS;
                         }
 
-                        if let Some(age) = character.age {
-                            if age < 200 {
-                                return RINGS;
-                            }
-
-                            panic!(" cannot be bulky, over 200 and humanoid");
-                        }
+                        // otherwise it has to be a dwarf
+                        return RINGS;
                     }
 
                     // any other trait is covered, blonde is either already filtered
@@ -121,7 +115,6 @@ fn process(character: &Character) -> Universe {
                 //      doesnt have planet nor any traits, cannot conlude
                 //  if age < 100
                 //      can be anything, cannot conclude
-                return UNKNOWN;
             } // is one of: marvel, hitch(covered by traits and, ring
             false => {
                 if let Some(traits) = maybe_traits {
@@ -147,10 +140,7 @@ fn process(character: &Character) -> Universe {
     return UNKNOWN;
 }
 
-//fn try_
-
 // classification related things
-#[allow(dead_code)]
 enum Universe {
     STAR_WARS,
     HITCH_HICKER,
@@ -159,103 +149,11 @@ enum Universe {
     UNKNOWN,
 }
 
-#[allow(dead_code)]
-struct Race_class {
-    name: String,
-    isHumanoid: bool,
-    planet: String,
-    age: Option<u32>,
-    traits: Vec<String>,
-}
-
-#[allow(dead_code)]
-struct Universe_class {
-    universe: Universe,
-    Races: Vec<Race_class>,
-}
-
-#[allow(dead_code)]
-fn setup_classifications() -> Vec<Universe_class> {
-    use Universe::*;
-
-    vec![
-        Universe_class {
-            universe: STAR_WARS,
-            Races: vec![
-                Race_class {
-                    name: "Wookie".to_string(),
-                    isHumanoid: false,
-                    planet: "Kashyyyk".to_string(),
-                    age: Some(400),
-                    traits: vec!["HAIRY".to_string(), "TALL".to_string()],
-                },
-                Race_class {
-                    name: "Ewok".to_string(),
-                    isHumanoid: false,
-                    planet: "Endor".to_string(),
-                    age: Some(60),
-                    traits: vec!["SHORT".to_string(), "HAIRY".to_string()],
-                },
-            ],
-        },
-        Universe_class {
-            universe: MARVEL,
-            Races: vec![Race_class {
-                name: "Asgardian".to_string(),
-                isHumanoid: true,
-                planet: "Asgard".to_string(),
-                age: Some(5000),
-                traits: vec!["BLONDE".to_string(), "TALL".to_string()],
-            }],
-        },
-        Universe_class {
-            universe: HITCH_HICKER,
-            Races: vec![
-                Race_class {
-                    name: "Betelgeusian".to_string(),
-                    isHumanoid: true,
-                    planet: "Betelgeuse".to_string(),
-                    age: Some(100),
-                    traits: vec!["EXTRA_ARMS".to_string(), "EXTRA_HEAD".to_string()],
-                },
-                Race_class {
-                    name: "Vogons".to_string(),
-                    isHumanoid: false,
-                    planet: "Vogsphere".to_string(),
-                    age: Some(200),
-                    traits: vec!["GREEN".to_string(), "BULKY".to_string()],
-                },
-            ],
-        },
-        Universe_class {
-            universe: RINGS,
-            Races: vec![
-                Race_class {
-                    name: "Ef".to_string(),
-                    isHumanoid: true,
-                    planet: "Earth".to_string(),
-                    age: None,
-                    traits: vec!["BLONDE".to_string(), "POINTY_EARS".to_string()],
-                },
-                Race_class {
-                    name: "Dwarf".to_string(),
-                    isHumanoid: true,
-                    planet: "Earth".to_string(),
-                    age: Some(200),
-                    traits: vec!["SHORT".to_string(), "BULKY".to_string()],
-                },
-            ],
-        },
-    ]
-}
-
 fn main() {
     // read the test-input.json
     let file_data = fs::read_to_string("resources/input.json").expect("failed to read file");
-    println!("{file_data}");
     let data: Data = serde_json::from_str(file_data.as_str()).expect("failed to parse json");
 
-    println!("json data: {data:?}");
     let mut star_wars: Vec<Character> = vec![];
     let mut hitch_hicker: Vec<Character> = vec![];
     let mut rings: Vec<Character> = vec![];
